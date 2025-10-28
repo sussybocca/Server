b6b7dec80730b04a2a58e39fadd53b47fe9198fc
sussybocca <babyyodacutefry@gmail.com>
2025-10-28 09:32:37 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+// js/main.js
+import { getServers } from './supabase.js'
 
+const serverList = document.getElementById('server-list')
+const container = document.getElementById('main-container')
+
+async function loadServers() {
+    const servers = await getServers()
+    serverList.innerHTML = ''
+
+    servers.forEach(server => {
+        const div = document.createElement('div')
+        div.className = 'server-card'
+        div.innerHTML = `<h3>Server ${server.id}</h3><button>Open</button>`
+        div.querySelector('button').addEventListener('click', () => loadServer(server))
+        serverList.appendChild(div)
+    })
+}
+
+function loadServer(server) {
+    container.innerHTML = `
+        <style>${server.css}</style>
+        ${server.html}
+        <script>${server.js}<\/script>
+    `
+}
+
+loadServers()
